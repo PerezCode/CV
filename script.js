e5bf8d5995a36d13c3dbf7da0e290298dbfc7e87
David Perez <davidagosto28@hotmail.com>
2019-08-23 14:50:46 -0500

    Actualicé la información del About Me, cambié los colores del background y agregué iconos nuevos

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let resume = document.getElementById("resume");
 let getResume = document.getElementById("getResume");
 let works = document.getElementById("works");
 let getWorks = document.getElementById("getWorks");
-let blog = document.getElementById("blog");
 let getBlog = document.getElementById("getBlog");
 let contact = document.getElementById("contact");
 let getContact = document.getElementById("getContact");
@@ -18,8 +17,6 @@ function remove() {
     getResume.classList.remove('selected');
     works.classList.remove('view');
     getWorks.classList.remove('selected');
-    blog.classList.remove('view');
-    getBlog.classList.remove('selected');
     contact.classList.remove('view');
     getContact.classList.remove('selected');
 }
@@ -49,14 +46,7 @@ getWorks.addEventListener('click', function (e) {
         getWorks.classList.add('selected');
     }
 })
-getBlog.addEventListener('click', function (e) {
-    if (window.innerWidth > 1040) {
-        e.preventDefault();
-        remove();
-        blog.classList.add('view');
-        getBlog.classList.add('selected');
-    }
-})
+
 getContact.addEventListener('click', function (e) {
     if (window.innerWidth > 1040) {
         e.preventDefault();
@@ -64,7 +54,6 @@ getContact.addEventListener('click', function (e) {
         contact.classList.add('view');
         getContact.classList.add('selected');
     }
-
 })
 
 var email = document.getElementById("email");
