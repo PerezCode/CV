0a47e97b521d1af7fbe74cdfd8c4d1f56e30836c
David Perez <davidagosto28@hotmail.com>
2019-08-27 17:54:44 -0500

    Se agregaron los modales

--- script.js
+++ script.js
@@ -7,8 +7,14 @@ let getWorks = document.getElementById("getWorks");
 let getBlog = document.getElementById("getBlog");
 let contact = document.getElementById("contact");
 let getContact = document.getElementById("getContact");
+let imagenes = document.querySelectorAll(".works-content img");
+let modales = document.querySelectorAll(".modal");
+let botonCerrar = document.querySelectorAll(".close-button");
 
-console.log(getResume)
+console.log(imagenes);
+console.log(modales);
+console.log(botonCerrar);
+
 
 function remove() {
     about.classList.remove('view');
@@ -19,7 +25,7 @@ function remove() {
     getWorks.classList.remove('selected');
     contact.classList.remove('view');
     getContact.classList.remove('selected');
-}
+};
 
 getAbout.addEventListener('click', function (e) {
     if (window.innerWidth > 1040) {
@@ -28,8 +34,8 @@ getAbout.addEventListener('click', function (e) {
         about.classList.add('view');
         getAbout.classList.add('selected');
     }
-
 });
+
 getResume.addEventListener('click', function (e) {
     if (window.innerWidth > 1040) {
         e.preventDefault();
@@ -37,7 +43,8 @@ getResume.addEventListener('click', function (e) {
         resume.classList.add('view');
         getResume.classList.add('selected');
     }
-})
+});
+
 getWorks.addEventListener('click', function (e) {
     if (window.innerWidth > 1040) {
         e.preventDefault();
@@ -45,7 +52,7 @@ getWorks.addEventListener('click', function (e) {
         works.classList.add('view');
         getWorks.classList.add('selected');
     }
-})
+});
 
 getContact.addEventListener('click', function (e) {
     if (window.innerWidth > 1040) {
@@ -54,7 +61,7 @@ getContact.addEventListener('click', function (e) {
         contact.classList.add('view');
         getContact.classList.add('selected');
     }
-})
+});
 
 var email = document.getElementById("email");
 var form = document.getElementById("form");
@@ -70,3 +77,68 @@ email.addEventListener("input", function (event) {
 form.addEventListener("submit", function (event) {
     event.preventDefault();
   });
+
+imagenes[0].addEventListener("click", function(){
+    modales[0].classList.add("modal-on");
+});
+
+imagenes[1].addEventListener("click", function(){
+    modales[1].classList.add("modal-on");
+});
+
+imagenes[2].addEventListener("click", function(){
+    modales[2].classList.add("modal-on");
+});
+
+imagenes[3].addEventListener("click", function(){
+    modales[3].classList.add("modal-on");
+});
+
+imagenes[4].addEventListener("click", function(){
+    modales[4].classList.add("modal-on");
+});
+
+imagenes[5].addEventListener("click", function(){
+    modales[5].classList.add("modal-on");
+});
+
+imagenes[6].addEventListener("click", function(){
+    modales[6].classList.add("modal-on");
+});
+
+imagenes[7].addEventListener("click", function(){
+    modales[7].classList.add("modal-on");
+});
+
+
+botonCerrar[0].addEventListener("click", function(){
+    modales[0].classList.remove("modal-on");
+});
+
+botonCerrar[1].addEventListener("click", function(){
+    modales[1].classList.remove("modal-on");
+});
+
+botonCerrar[2].addEventListener("click", function(){
+    modales[2].classList.remove("modal-on");
+});
+
+botonCerrar[3].addEventListener("click", function(){
+    modales[3].classList.remove("modal-on");
+});
+
+botonCerrar[4].addEventListener("click", function(){
+    modales[4].classList.remove("modal-on");
+});
+
+botonCerrar[5].addEventListener("click", function(){
+    modales[5].classList.remove("modal-on");
+});
+
+botonCerrar[6].addEventListener("click", function(){
+    modales[6].classList.remove("modal-on");
+});
+
+botonCerrar[7].addEventListener("click", function(){
+    modales[7].classList.remove("modal-on");
+});
